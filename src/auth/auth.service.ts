6f4d7e8082839969d374c419067fb330dd7004c4
Nikita Martynov <martydev@mail.ru>
2024-12-23 15:50:57 +0400

    add: addToken and removeToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,9 +8,13 @@ import { JwtService } from '@nestjs/jwt'
 import { verify } from 'argon2'
 import { UserService } from 'src/user/user.service'
 import { AuthDto } from './dto/auth.dto'
+import { Response } from 'express'
 
 @Injectable()
 export class AuthService {
+	EXPIRE_DAY_REFRESH_TOKEN = 1
+	REFRESH_TOKEN_NAME = 'refreshToken'
+
 	constructor(
 		private jwt: JwtService,
 		private userService: UserService
@@ -65,5 +69,31 @@ export class AuthService {
 		if (!isValid) throw new UnauthorizedException('Invalid password')
 
 		return user
+	}
+
+	addRefreshTokenToResponse(res: Response, refreshToken: string) {
+
+		const expiresIn = new Date()
+		expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+
+		res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+			httpOnly: true,
+			domain: 'localhost',
+			expires: expiresIn,
+			secure: true,
+			// lax if production
+			sameSite: 'none'
+		})
+	}
+
+	removeRefreshTokenFromResponse(res: Response) {
+		res.cookie(this.REFRESH_TOKEN_NAME, '', {
+			httpOnly: true,
+			domain: 'localhost',
+			expires: new Date(0),
+			secure: true,
+			// lax if production
+			sameSite: 'none'
+		})
 	}
 }
