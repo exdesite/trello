833acb4c822a48a7bbda0023600008790ecd7ceb
Nikita Martynov <martydev@mail.ru>
2024-12-24 13:52:39 +0400

    add: task

---
+++ src/task/task.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma.service'
+import { TaskDto } from './task.dto'
+
+@Injectable()
+export class TaskService {
+	constructor(private prisma: PrismaService) {}
+
+	getById(id: string) {
+		return this.prisma.task.findUnique({
+			where: {
+				id
+			}
+		})
+	}
+
+	async create(dto: TaskDto) {
+		return this.prisma.task.create({
+			data: dto
+		})
+	}
+
+	async update(id: string, dto: TaskDto) {}
+}
