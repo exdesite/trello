0158880651b89a4b4c6c4b411ad5068f03cef965
Nikita Martynov <martydev@mail.ru>
2024-12-24 12:47:30 +0400

    add: user dto and endpoint

--- src/main.ts
+++ src/main.ts
@@ -16,5 +16,4 @@ async function bootstrap() {
   await app.listen(3000);
 }
 
-
 bootstrap();
--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,8 +1,45 @@
-import { IsNumber, IsOptional, Min } from 'class-validator'
+import {
+	IsEmail,
+	IsNumber,
+	IsOptional,
+	IsString,
+	Max,
+	Min,
+	MinLength
+} from 'class-validator'
 
 export class PomodoroSettingsDto {
+	@IsOptional()
+	@IsNumber()
+	@Min(1)
+	workInterval?: number
+
+	@IsOptional()
+	@IsNumber()
+	@Min(1)
+	breakInterval?: number
+
+	@IsOptional()
+	@IsNumber()
+	@Min(1)
+	@Max(10)
+	intervalCount?: number
+}
+
+export class UserDto extends PomodoroSettingsDto {
+	@IsEmail()
     @IsOptional()
-    @IsNumber()
-    @Min(1)
-    workInterval?: number
+	email?: string
+
+    @IsString()
+    @IsOptional()
+	name?: string
+
+    @IsOptional()
+	@MinLength(6, {
+		message: 'Password must be at least 6 characters long'
+	})
+
+	@IsString()
+	password?: string
 }
